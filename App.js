654a8cf954e07563fa1758dc38a763fc24923258
Adarsh Gupta <adarshgupta4399@gmail.com>
2021-07-22 23:43:36 +0530

    Normal Calculator made scntfc remains

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import NumPad from "./components/NumPad";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={styles.safe}>
+        <NumPad />
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  safe: {
+    flex: 1,
+    justifyContent: "space-around"
+  }
 });
